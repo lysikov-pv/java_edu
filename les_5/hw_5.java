5ee32f5bbf7e0baa731864b7e16cd7a1baa00161
Petr Lysikov <p.v.lysikov@gmail.com>
2023-06-28 09:51:09 +0500

    ДЗ5 в процессе

--- les_5/hw_5.java
+++ les_5/hw_5.java
@@ -1,9 +1,16 @@
+// Реализуйте структуру телефонной книги с помощью HashMap.
+// Программа также должна учитывать, что во входной структуре будут повторяющиеся имена с разными
+// телефонами, их необходимо считать, как одного человека с разными телефонами. Вывод должен быть
+// отсортирован по убыванию числа телефонов.
+
 package les_5;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Collection;
+import java.util.Collections;
 
 public class hw_5 {
     public static Map<String, ArrayList<String>> phonebook = new HashMap<>();
@@ -13,12 +20,14 @@ public class hw_5 {
             phonebook.get(name).add(phone);
         }
         else {
-            //ArrayList<String> new_phones = new ArrayList<>();
-            //new_phones.add(phone);
-            //phonebook.put(name, new_phones);
             phonebook.put(name, new ArrayList<String>(Arrays.asList(phone.split("\n"))));
         }
     }
+    
+    public static void sortByPhones(Map<String, ArrayList<String>> map) {
+        // Collections.sort(map, (o1, o2) -> o1.getValue().size().compareTo(o2.getValue().size()));
+    }
+
     public static void print_records(Map<String, ArrayList<String>> map) {
         map.forEach((key, value) -> System.out.println(key + " " + value));
 
