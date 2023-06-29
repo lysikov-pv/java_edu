7f06b89d413288615af72f9102978d7b3c2e68d4
Petr Lysikov <p.v.lysikov@gmail.com>
2023-06-29 10:59:00 +0500

    ДЗ5

--- les_5/hw_5.java
+++ les_5/hw_5.java
@@ -8,6 +8,8 @@ package les_5;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Collection;
 import java.util.Collections;
@@ -24,18 +26,37 @@ public class hw_5 {
         }
     }
     
-    public static void sortByPhones(Map<String, ArrayList<String>> map) {
-        // Collections.sort(map, (o1, o2) -> o1.getValue().size().compareTo(o2.getValue().size()));
+    // public static Map<String, ArrayList<String>> sortByPhones(Map<String, ArrayList<String>> map) {
+    //     List<Map.Entry<String, ArrayList<String>>> entries = new ArrayList<>(map.entrySet());
+    //     entries.sort((e1, e2) -> e2.getValue().size() - e1.getValue().size());
+         
+ 
+    //     Map<String, ArrayList<String>> result = new LinkedHashMap<>();
+    //     for (Map.Entry<String, ArrayList<String>> entry : entries) {
+    //         result.put(entry.getKey(), entry.getValue());
+    //     }
+    //     return result;
+    // }
+
+    public static List<Map.Entry<String, ArrayList<String>>> sortByPhones2(Map<String, ArrayList<String>> map) {
+        List<Map.Entry<String, ArrayList<String>>> entries = new ArrayList<>(map.entrySet());
+        entries.sort((e1, e2) -> e2.getValue().size() - e1.getValue().size());
+        return entries;
     }
 
     public static void print_records(Map<String, ArrayList<String>> map) {
         map.forEach((key, value) -> System.out.println(key + " " + value));
+    }
 
+    public static void print_records(List<Map.Entry<String, ArrayList<String>>> entries) {
+        for (Map.Entry<String, ArrayList<String>> entry : entries) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
     }
     public static void main(String[] args) {
         add_record("Ivanov", "+7123");
         add_record("Petrov", "+7567");
         add_record("Ivanov", "+7321");
-        print_records(phonebook);
+        print_records(sortByPhones2(phonebook));
     }
 }
