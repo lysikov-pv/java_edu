536c1a2cc544bfcb046e4c16979e2d1af66c94a7
Petr Lysikov <p.v.lysikov@gmail.com>
2023-07-06 00:23:06 +0500

    ООП ДЗ №1-3

--- oop/Archer.java
+++ oop/Archer.java
@@ -2,11 +2,18 @@ package oop;
 
 public class Archer extends Shooter {
     protected static int number;
+    private static final int MAX_HP = 10;
+    private static final int ATTACK = 6;
+    private static final int DEFENSE = 3;
+    private static final int MIN_DAMAGE = 2;
+    private static final int MAX_DAMAGE = 3;
+    private static final int COST = 100;
+    private static final int MAX_SHOOTS = 12;
     /**
      * Конструктор
      * @param qty Количество
      */
     public Archer(int qty) {
-        super(++Archer.number, "Арбалетчик", qty, 10, 6, 3, 2, 3, 100, 12);
+        super(++Archer.number, "Арбалетчик", qty, MAX_HP, ATTACK, DEFENSE, MIN_DAMAGE, MAX_DAMAGE, COST, MAX_SHOOTS);
     }
 }
