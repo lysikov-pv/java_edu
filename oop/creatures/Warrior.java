0ef2c6e6afb64b0119ffe50602e28fb067a0da9a
Petr Lysikov <p.v.lysikov@gmail.com>
2023-07-26 09:14:07 +0500

    ООП ДЗ #6 (rev2)

---
+++ oop/creatures/Warrior.java
@@ -0,0 +1,114 @@
+package oop.creatures;
+
+import oop.view.View;
+
+import java.util.ArrayList;
+
+import static oop.Program.allCreatures;
+
+public abstract class Warrior extends Creature {
+
+    /**
+     * Конструктор
+     *
+     * @param qty        Количество
+     * @param x          Координата Х
+     * @param y          Координата Y
+     * @param number     Порядковый номер
+     * @param name       Название существа
+     * @param maxHp      Максимальное здоровье
+     * @param attack     Атака
+     * @param defense    Защита
+     * @param minDamage  Минимальный урон
+     * @param maxDamage  Максимальный урон
+     * @param cost       Стоимость
+     * @param initiative Инициатива
+     */
+    protected Warrior(int qty, int x, int y, int number, String name, int maxHp, int attack, int defense, int minDamage, int maxDamage, int cost, int initiative) {
+        super(qty, x, y, number, name, maxHp, attack, defense, minDamage, maxDamage, cost, initiative);
+
+    }
+    public void goTo(Position target){
+        int x = getPosition().x;
+        int y = getPosition().y;
+        int dy = y - target.y;
+        Position top    = new Position(x, (dy > 0) ? y + 1 : y - 1);
+        Position bottom = new Position(x, (dy > 0) ? y - 1 : y + 1);
+        Position left   = new Position(x - 1, y);
+        Position right  = new Position(x + 1, y);
+        boolean topFree    = true;
+        boolean bottomFree = true;
+        boolean leftFree   = true;
+        boolean rightFree  = true;
+
+        if (top.y > 10)   topFree    = false;
+        if (bottom.y < 1) bottomFree = false;
+        if (left.x < 1)   leftFree   = false;
+        if (right.x > 10) rightFree  = false;
+
+        for (Creature creature: allCreatures) {
+            if(creature.getPosition().equals(top))    topFree    = false;
+            if(creature.getPosition().equals(bottom)) bottomFree = false;
+            if(creature.getPosition().equals(left))   leftFree   = false;
+            if(creature.getPosition().equals(right))  rightFree  = false;
+        }
+
+        double[] distances = {top.getDistance(target),
+                bottom.getDistance(target),
+                left.getDistance(target),
+                right.getDistance(target)};
+        boolean[] positionFree = {topFree, bottomFree, leftFree, rightFree};
+
+        double minDistance = -1;
+        int newPositionIndex = -1;
+        for (int i = 0; i < distances.length; i++) {
+            if ((minDistance == -1 || minDistance > distances[i]) && positionFree[i]) {
+                minDistance = distances[i];
+                newPositionIndex = i;
+            }
+        }
+        if (newPositionIndex == 0) {
+            position.x = top.x;
+            position.y = top.y;
+        }
+        else if (newPositionIndex == 1) {
+            position.x = bottom.x;
+            position.y = bottom.y;
+        }
+        else if (newPositionIndex == 2) {
+            position.x = left.x;
+            position.y = left.y;
+        }
+        else if (newPositionIndex == 3) {
+            position.x = right.x;
+            position.y = right.y;
+        }
+    }
+
+    protected boolean canAttack(Creature creature){
+        return (Math.abs(getPosition().x - creature.getPosition().x) <= 1)
+                && (Math.abs(getPosition().y - creature.getPosition().y) <= 1);
+    }
+
+    @Override
+    public void step(ArrayList<Creature> enemies, ArrayList<Creature> allies) {
+        if (hp <= 0) return;
+
+        if (findNearest(enemies) != null) {
+            Creature nearestEnemy = findNearest(enemies);
+            if (canAttack(nearestEnemy)) {
+                int damage = ((maxDamage - minDamage) / 2 + minDamage) * qty;
+                nearestEnemy.doDamage(damage);
+                View.logMessage.addLast(getInfo() + String.format(" -> Атаковал: %s #%d и нанес урон: %d",
+                        nearestEnemy.name,
+                        nearestEnemy.number,
+                        damage));
+            } else {
+                goTo(nearestEnemy.getPosition());
+                View.logMessage.addLast(getInfo() + String.format(" -> Сделал шаг. Новые координаты: (%d, %d)",
+                        getPosition().x,
+                        getPosition().y));
+            }
+        }
+    }
+}
