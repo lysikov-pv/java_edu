c6c86263a546ea613bb1b320284e19c318f0cba5
Petr Lysikov <p.v.lysikov@gmail.com>
2023-08-02 00:09:50 +0500

    В латиницу.

--- basic/les_6/t/CatsName.java
+++ basic/les_6/t/CatsName.java
@@ -3,7 +3,7 @@ package basic.les_6.t;
 import java.util.Random;
 
 public enum CatsName {
-    Барсик, Персик, Уголек, Снежок, Мурзик;
+    Barsik, Persik, Ugolek, Snezhok, Murzik;
 
     public static String get(){
         return String.valueOf(CatsName.values()[new Random().nextInt(CatsName.values().length)]);
